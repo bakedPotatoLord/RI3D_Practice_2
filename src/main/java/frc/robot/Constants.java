b2cfa4a82239d7e9d035ff395374ba0b1bfe9ff9
bakedpotatolord <jeffy9745@gmail.com>
2025-11-08 14:51:48 -0700

    start on elevator subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -60,5 +60,48 @@ public final class Constants {
     }
   }
 
-  public static class ElevatorConstants
+  public static class ElevatorConstants{
+
+    // first is main, second is follower
+    public static final int[] ELEVATOR_CAN_ID = {5,6};
+    public static final boolean[] INVERTED = {false,true};
+
+    public static final int CURRENT_LIMIT = 50;
+
+    public static final SparkBaseConfig mainConfig = new SparkMaxConfig();
+    public static final SparkBaseConfig followerConfig = new SparkMaxConfig();
+
+    public static final int LIMIT_SWITCH_DIO_PORT = 0;
+
+    public static final double SETPOINT_BASE = 0,
+      SETPOINT_L1 = 0.5,
+      SETPOINT_L2 = 1.5,
+      SETPOINT_L3 = 2.5,
+      SETPOINT_L4 = 3.5;
+
+    public static final double ELEVATOR_ZERO_SPEED = -0.2;
+
+    public enum ELEVATOR_STATE{
+      BASE,
+      L1,
+      L2,
+      L3,
+      L4
+    }
+
+    static{
+      mainConfig.idleMode(IdleMode.kBrake)
+      .inverted(INVERTED[0])
+      .smartCurrentLimit(CURRENT_LIMIT);
+      mainConfig.closedLoop.pidf(0.2, 0, 0, 1)
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      .outputRange(-1, 1);
+
+      followerConfig.idleMode(IdleMode.kBrake)
+      .inverted(INVERTED[1])
+      .smartCurrentLimit(CURRENT_LIMIT)
+      .follow(ELEVATOR_CAN_ID[0]);
+    }
+
+  }
 }
